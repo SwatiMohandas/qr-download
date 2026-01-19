d0b21599b8db179f902e14c52406fa67cde5405d
Swati Mohandas <swati.mohandas16@gmail.com>
2026-01-19 15:46:47 +0530

    Updated URL

--- app/page.tsx
+++ app/page.tsx
@@ -4,16 +4,23 @@ import { QRCodeCanvas } from "qrcode.react";
 
 export default function Home() {
   const url =
-    typeof window !== "undefined"
-      ? `${window.location.origin}/download`
-      : "http://localhost:3000/download";
+    typeof window !== "undefined" ?
+      `${window.location.origin}/download`
+    : "https://qr-download-one.vercel.app/";
 
   return (
     <main style={{ padding: 24, fontFamily: "sans-serif" }}>
       <h1>Scan to Download</h1>
       <p>Scan this QR code to download 2 PDFs and 1 MP4.</p>
 
-      <div style={{ marginTop: 20, background: "#fff", padding: 16, display: "inline-block" }}>
+      <div
+        style={{
+          marginTop: 20,
+          background: "#fff",
+          padding: 16,
+          display: "inline-block",
+        }}
+      >
         <QRCodeCanvas value={url} size={260} includeMargin />
       </div>
 
